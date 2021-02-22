c3a0fcbe60be2cb269faba8530eeadec35708aac
Lukas Stengel <derdiador@gmail.com>
2021-02-22 10:26:41 +0100

    Added Descirption

--- src/contentTest/Main.java
+++ src/contentTest/Main.java
@@ -3,10 +3,7 @@ package contentTest;
 public class Main {
 
     public static void main(String[] args) {
-        Console.print("Hello World!");
+        // Die Klasse kann eignentlich gelöscht werden
 
-        Programm.sleep(1000);
-
-        Console.print("Dieser Text ist erst nach einer Sekunde erschienen!");
     }
 }
--- src/contentTest/Programm.java
+++ src/contentTest/Programm.java
@@ -2,6 +2,11 @@ package contentTest;
 
 public class Programm {
 
+    // Dont let initialize the Class
+    private Programm() {
+    }
+
+    // Causes the currently executing thread to sleep (temporarily cease execution) for the specified number of milliseconds, subject to the precision and accuracy of system timers and schedulers. The thread does not lose ownership of any monitors.
     public static void sleep(final int millis) {
         try { Thread.sleep(millis); } catch (InterruptedException e) { Console.print("[Programm: sleep] > Something went wrong:"); }
     }
